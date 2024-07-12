45b2c1929cd80a1d00fe1b46dfce7e492ce808ec
Necati Meral <necati_meral@yahoo.de>
2024-07-13 01:09:53 +0200

    Improve error handling (#11)
    * Improve error handling
    
    * Throw
    
    * wip

--- dotnet/src/NecatiMeral.Logic.Meltem/MeltemNodeBase.cs
+++ dotnet/src/NecatiMeral.Logic.Meltem/MeltemNodeBase.cs
@@ -78,15 +78,15 @@ public abstract class MeltemNodeBase : LocalizableNode
 
     protected void ExecuteWithConnection(Action<ModbusClient> action)
     {
-        _client.Connect(IPAddress.Value, Port.Value);
-        _client.UnitIdentifier = (byte)UnitId.Value;
-        if (!_client.Connected)
-        {
-            return;
-        }
-
         try
         {
+            _client.Connect(IPAddress.Value, Port.Value);
+            _client.UnitIdentifier = (byte)UnitId.Value;
+            if (!_client.Connected)
+            {
+                throw new InvalidOperationException("Connection failed");
+            }
+
             action(_client);
         }
         finally
